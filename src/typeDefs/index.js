220d79bd2c7704255b55f6a37e0066f4df651929
Edward CG Cheah <edward_cg_cheah@manulife.ca>
2019-08-20 10:03:54 -0400

    update typeDefs' index.js

--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -4,4 +4,4 @@ const { importSchema } = require('graphql-import');
 
 const schema = importSchema(path.resolve(__dirname, './schema.graphql'));
 
-module.exports = [gql(schema)];
+module.exports = gql(schema);
